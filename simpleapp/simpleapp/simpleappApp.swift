913372f9e27fa2b542876829065b2dc83f784731
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-10-31 19:44:51 +0600

    Initial commit

---
+++ simpleapp/simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by Gaming Lab on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
